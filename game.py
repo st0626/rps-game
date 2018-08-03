575ddcef27ff5257fd66436d332116bf5b43a447
Byron Tollison <st06266@gmail.com>
2018-08-02 19:03:33 -0500

    condensed code

--- game.py
+++ game.py
@@ -4,17 +4,16 @@ import random
 while True:
     user_pick = input('Pick rock(1), paper(2), or scissors(3) or type "end" to end the game:  ')
     
-    if user_pick == '1':
-        print('you picked rock!')
-    elif user_pick == '2':
-        print('you picked paper!')
-    elif user_pick == '3':
-        print('you picked scissors!')
+    options = ["rock!", "paper!", "scissors!"]
+    if int(user_pick) > len(options) and int(user_pick) > 0:
+        print("you didn't pick a number?!")
     else:
-        print("you didn't pick a number?!!")
+        print("you picked", options[int(user_pick)])
     
     computer_pick = ['rock', 'paper', 'scissors']
     print('Computer picks ' + random.choice(computer_pick) + "!")
     
-    if user_pick == "end":
-        break
+    
+
+
+    
